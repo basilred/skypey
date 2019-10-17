18edab3944bbf97eebf2f03b1c979d5ab2e0d5ee
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-18 00:11:31 +0300

    We got a data

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './Sidebar.css';
 
-const Sidebar = () => <aside className="Sidebar">Sidebar</aside>;
+const Sidebar = () => (
+  <aside className="Sidebar">
+    {/* {contacts.map(contact => <User/>)} */}
+    Sidebar
+  </aside>
+);
 
 export default Sidebar;
