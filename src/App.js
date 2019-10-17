32d7f4eb3fa9955ed3f0db91ee229a067a20ae79
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-18 00:24:29 +0300

    Pass contacts as array instead of object

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import Main from './Main';
 import './App.css';
+import store from './store';
 
 function App() {
+  const { contacts } = store.getState();
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar contacts={Object.values(contacts)} />
       <Main />
     </div>
   );
