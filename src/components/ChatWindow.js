e7f5aead280b5f7524f7c81edd39917f33bda35b
Vassily Krasnov <mykrasnov@gmail.com>
2019-10-22 23:24:42 +0300

    Make simple component for ChatWindow

---
+++ src/components/ChatWindow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatWindow = ({ activeUserId }) => (
+  <div className="ChatWindow">
+    Conversation for user id:
+    {' '}
+    {activeUserId}
+  </div>
+);
+
+export default ChatWindow;
